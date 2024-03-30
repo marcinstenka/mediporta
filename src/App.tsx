41ce041f6e89799c8e3d35568f201f7bc06ab0d4
marcinstenka <mastenka@wp.pl>
2024-03-30 08:58:52 +0100

    components reorgaznied

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import './App.css';
 import Table from './lib/components/Table.tsx';
+import Inputs from './lib/components/Inputs.tsx';
+import TableArrows from './lib/components/TableArrows.tsx';
 
 function App() {
-	return <Table />;
+	return (
+		<>
+			<Inputs />
+			<Table />
+			<TableArrows />
+		</>
+	);
 }
 
 export default App;
--- src/lib/components/Table.tsx
+++ src/lib/components/Table.tsx
@@ -6,42 +6,32 @@ import {
 	TableCell,
 	TableContainer,
 	TableHead,
-	TablePagination,
 	TableRow,
 } from '@mui/material';
 import useFetch from '../hooks/useFetch.ts';
-import Inputs from './Inputs.tsx';
-import TableArrows from './TableArrows.tsx';
 
 export default function TagTable() {
 	const rows = useFetch();
 
 	if (!rows) return <h2>Loading...</h2>;
 	return (
-		<>
-			<Inputs />
-			<TableContainer
-				component={Paper}
-				sx={{ maxWidth: 400, margin: '0 auto' }}
-			>
-				<Table aria-label='simple table'>
-					<TableHead>
+		<TableContainer component={Paper} sx={{ maxWidth: 400, margin: '0 auto' }}>
+			<Table aria-label='simple table'>
+				<TableHead>
+					<TableRow>
+						<TableCell sx={{ fontWeight: 'bold' }}>Name</TableCell>
+						<TableCell sx={{ fontWeight: 'bold' }}>Count</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => (
 						<TableRow>
-							<TableCell sx={{ fontWeight: 'bold' }}>Name</TableCell>
-							<TableCell sx={{ fontWeight: 'bold' }}>Count</TableCell>
+							<TableCell>{row.name}</TableCell>
+							<TableCell>{row.count}</TableCell>
 						</TableRow>
-					</TableHead>
-					<TableBody>
-						{rows.map((row) => (
-							<TableRow>
-								<TableCell>{row.name}</TableCell>
-								<TableCell>{row.count}</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
-			<TableArrows />
-		</>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
 	);
 }
