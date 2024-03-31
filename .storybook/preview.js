5ca5c33a7bd6db570605bc437fa2d3a5cd922126
marcinstenka <mastenka@wp.pl>
2024-03-31 22:28:29 +0200

    Storybook working & inputs story added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,19 @@
 /** @type { import('@storybook/react').Preview } */
+import TableContentContextProvider from '../src/lib/context/tableContentContext.tsx';
+import TableOptionsContextProvider from '../src/lib/context/tableOptionsContext.tsx';
+import '../src/App.css';
+
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+	parameters: {},
+	decorators: [
+		(Story) => (
+			<TableContentContextProvider>
+				<TableOptionsContextProvider>
+					<Story />
+				</TableOptionsContextProvider>
+			</TableContentContextProvider>
+		),
+	],
 };
 
 export default preview;
---
+++ src/stories/Table.stories.js
@@ -0,0 +1,8 @@
+import Inputs from '../lib/components/Inputs.tsx';
+
+export default {
+	title: 'Inputs',
+	component: Inputs,
+};
+const Template = () => <Inputs></Inputs>;
+export const Normal = Template.bind({});
